609c78cc8fc103de07742a7bcf2fbcf738c343b9
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-23 15:22:29 +0200

    Update Parametric Equation Circle.py

--- Parametric Equation Circle.py
+++ Parametric Equation Circle.py
@@ -2,8 +2,8 @@ import turtle
 import math
 r = 100
 for t in range(1,1000):
-    x = r*math.cos(t/math.pi)
-    y = r*math.sin(t/math.pi)
+    x = r*math.cos(t)/math.pi)
+    y = r*math.sin(t)/math.pi)
     turtle.setposition(x,y)
 
 
