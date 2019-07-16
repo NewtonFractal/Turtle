03f2854fb3d44605fd3772af4b22963c465b5874
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-16 16:43:42 +0200

    Create Triforce.py

---
+++ Triforce.py
@@ -0,0 +1,63 @@
+import turtle
+import math
+
+coordinate_list_x = []
+coordinate_list_y = []
+
+def Sierpinski(iterations):
+    x = coordinate_list_x[-3]
+    y = coordinate_list_y[-3]
+    x = coordinate_list_x[-3]
+    y = coordinate_list_y[-3]
+    x = coordinate_list_x[-3]
+    y = coordinate_list_y[-3]
+
+
+def initial(iterations):
+    x = 0
+    y = 0
+    coordinate_list_x.append(x)
+    coordinate_list_y.append(y)
+    turtle.setposition(x, y)
+    x += (25 / 2)*iterations
+    coordinate_list_x.append(x)
+    y += (25 * math.sqrt(3) / 2) * iterations
+    coordinate_list_y.append(y)
+    turtle.setposition(x, y)
+    x += (25 / 2) * iterations
+    coordinate_list_x.append(x)
+    y -= (25 * math.sqrt(3) / 2) *iterations
+    coordinate_list_y.append(y)
+    turtle.setposition(x, y)
+    x = coordinate_list_x[-3]
+    y = coordinate_list_y[-3]
+    coordinate_list_y.append(y)
+    coordinate_list_x.append(x)
+    turtle.setposition(x, y)
+    for z in range(0,1):
+        x = coordinate_list_x[-4] +(coordinate_list_x[-3] - coordinate_list_x[-4])/2
+        y = coordinate_list_y[-4] + (coordinate_list_y[-3] - coordinate_list_y[-4]) / 2
+        coordinate_list_x.append(x)
+        coordinate_list_y.append(y)
+        turtle.setposition(x, y)
+        x = coordinate_list_x[-4] +(coordinate_list_x[-3] - coordinate_list_x[-4])/2
+        y = coordinate_list_y[-4] + (coordinate_list_y[-3] - coordinate_list_y[-4]) / 2
+        coordinate_list_x.append(x)
+        coordinate_list_y.append(y)
+        turtle.setposition(x, y)
+        x = coordinate_list_x[-4] +(coordinate_list_x[-3] - coordinate_list_x[-4])/2
+        y = coordinate_list_y[-4] + (coordinate_list_y[-3] - coordinate_list_y[-4]) / 2
+        coordinate_list_x.append(x)
+        coordinate_list_y.append(y)
+        turtle.setposition(x, y)
+        x = coordinate_list_y[5]
+        y = coordinate_list_y[5]
+        turtle.setposition(x, y)
+        Sierpinski(iterations)
+
+
+
+initial(3)
+
+print(coordinate_list_x)
+print(coordinate_list_y)
